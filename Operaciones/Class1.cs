f6eb94ce97d47fbde0d8239d3e6d4b3177f7414e
KevinMoscoso <sebastianbaculima1@gmail.com>
2024-05-01 23:13:06 -0500

    operaciones commit

--- Operaciones/Class1.cs
+++ Operaciones/Class1.cs
@@ -1,6 +1,17 @@
 ﻿namespace Operaciones;
 
-public class Class1
+public class operacionesBasicas
 {
-
+    public int suma(int a, int b) //funcion para sumar
+    {
+        return a + b; //retorna el resultado de la suma
+    }
+    public int Restar(int a, int b) //funsión para restar
+    {
+        return a - b; //retorna el resultado de la resta
+    }
+    public int multiplicacion(int a, int b) //funcion para multiplicar
+    {
+        return a * b; //retorna el resultado de la multiplicación
+    }
 }
--- testOperaciones/UnitTest1.cs
+++ testOperaciones/UnitTest1.cs
@@ -2,9 +2,20 @@ namespace testOperaciones;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(2, 3, 5)] //Prueba con datos fijos
+    [InlineData(4, 3, 7)]
+    [InlineData(8, 11, 19)]
+    [InlineData(1, 1, 2)]
+    [InlineData(212, 35, 247)]
+    public void TestSuma(int a, int b, int resultadoEsperado)
     {
-
+        ///AAA Arrange Act Assert
+        ///Arrange: Preparar los datos de entrada
+        int resultadoReal;
+        ///Act: Invocar al metodo
+        resultadoReal=operacionesBasicas.suma(a,b);
+        ///Assert: verificar el resultado sea lo esperado
+        Assert.Equal(resultadoEsperado, resultadoReal)
     }
 }
